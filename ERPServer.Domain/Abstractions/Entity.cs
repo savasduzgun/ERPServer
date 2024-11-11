ed008158a985718871d645b6bf8c6c510a3f54e9
savasduzgun <savasdzgn@gmail.com>
2024-11-11 23:42:54 +0300

    Domanin-Entities-Entity için açıklama yazıldı.

--- ERPServer.Domain/Abstractions/Entity.cs
+++ ERPServer.Domain/Abstractions/Entity.cs
@@ -2,6 +2,7 @@
 {
     public abstract class Entity
     {
+        //default entitylerin ekleneceği id alanı, abstract hali, bu sayede default entitylerde product, category vb. inherit alarak bunu kullanabiliriz id alanı için
         public Guid Id { get; set; }
         protected Entity()
         {
