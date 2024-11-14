a232e80f3d516381d5eacd789360ba2102fcdbc6
savasduzgun <savasdzgn@gmail.com>
2024-11-14 23:57:18 +0300

    Aplication-Features-Auth-Login-LoginCommand için açıklama yazıldı.

--- ERPServer.Application/Features/Auth/Login/LoginCommand.cs
+++ ERPServer.Application/Features/Auth/Login/LoginCommand.cs
@@ -3,6 +3,7 @@ using TS.Result;
 
 namespace ERPServer.Application.Features.Auth.Login
 {
+    //hem mail hem username ile giriş yapabilmek için requeste istekte bulunan method
     public sealed record LoginCommand(
         string EmailOrUserName,
         string Password) : IRequest<Result<LoginCommandResponse>>;
